4f82cd57b9b3374c7448114367a84e85d74d19a3
Dylan Smith <dylan1999.smith@gmail.com>
2019-09-09 20:48:06 -0500

    Created a REST API that creates a user and displays a name

---
+++ Experiments/DylanSmithExperiment/BackendExperiment/src/com/starritt/contract/dto/Contract.java
@@ -0,0 +1,27 @@
+package com.starritt.contract.dto;
+
+public class Contract {
+    private String name;
+    private int id;
+
+    public void setName(String setName)
+    {
+        name = setName;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setId(int setId)
+    {
+        id = setId;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+}
