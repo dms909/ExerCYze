e0d0d9a55b755775654fa52eaa29c50ce460a5c6
nfred99 <nfred99@iastate.edu>
2019-09-18 11:49:29 -0500

    First design for frontend of app

--- Frontend/AppMenu/app/src/main/java/com/h/mainappmenu/MainActivity.java
+++ Frontend/AppMenu/app/src/main/java/com/h/mainappmenu/MainActivity.java
@@ -9,6 +9,7 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,24 +17,26 @@ import android.view.View;
 import com.h.mainappmenu.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private SectionsPagerAdapter mSectionsPageAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        Log.d(TAG, "onCreate: Starting.");
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //mSectionsPageAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment(), "TAB1");
+        adapter.addFragment(new Tab2Fragment(), "TAB2");
+        adapter.addFragment(new Tab3Fragment(), "TAB3");
+        viewPager.setAdapter(adapter);
     }
 }
