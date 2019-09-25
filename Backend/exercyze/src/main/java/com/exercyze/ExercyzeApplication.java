0654a8da9ef27501a2e4e9919fdaab70f5fc268b
Dylan Smith <dylan1999.smith@gmail.com>
2019-09-25 17:05:42 -0500

    Backend

---
+++ Backend/exercyze/src/main/java/com/exercyze/ExercyzeApplication.java
@@ -0,0 +1,13 @@
+package com.exercyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercyzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercyzeApplication.class, args);
+	}
+
+}
