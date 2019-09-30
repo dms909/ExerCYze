381597c67d551956c9dc73134ce9c252d5165d87
nfred99 <nfred99@iastate.edu>
2019-09-30 11:12:44 -0500

    Exercyze app setup

--- Frontend/exeryze/app/src/main/java/com/example/exercyze/MainActivity.java
+++ Frontend/exeryze/app/src/main/java/com/example/exercyze/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.exercyze;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button bLogout;
+    EditText etName, etAge, etUsername, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etAge = (EditText) findViewById(R.id.etAge);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+
+        bLogout = (Button) findViewById(R.id.bLogout);
+
+        bLogout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bLogout:
+
+                startActivity(new Intent(this, Login.class));
+                break;
+        }
     }
 }
