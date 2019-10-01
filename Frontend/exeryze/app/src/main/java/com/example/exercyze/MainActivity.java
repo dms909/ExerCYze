637a9c34deb1a85cfa7e4db84e69dbf48f09f097
mspatel <mspatel@iastate.edu>
2019-10-01 18:33:26 -0500

    volley added

--- Frontend/exeryze/app/src/main/java/com/example/exercyze/MainActivity.java
+++ Frontend/exeryze/app/src/main/java/com/example/exercyze/MainActivity.java
@@ -2,11 +2,22 @@ package com.example.exercyze;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.exercyze.app.AppController;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity  {
 
@@ -41,5 +52,25 @@ public class MainActivity extends AppCompatActivity  {
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
     }
+
+
+    // Tag used to cancel the request
+    String tag_json_obj ="json_obj_req";
+    String url ="https://coms-309-sb-7.misc.iastate.edu/api/users";
+
+    JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+        @Override
+                public void onResponse(JSONObject response){
+            Log.d(TAG, response.toString());
+        }
+    },new Response.ErrorListener() {
+        @Override
+                public void onErrorResponse(VolleyError error) {
+            VolleyLog.d(TAG,"Error: "+ error.getMessage());
+
+        }
+    });
+        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+
 
 }
