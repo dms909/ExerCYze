57b8dc0c31e8444a2ca4a1345b1b2b5fcc50e641
mspatel <mspatel@iastate.edu>
2019-10-01 19:17:45 -0500

    request to url is set

--- Frontend/exeryze/app/src/main/java/com/example/exercyze/MainActivity.java
+++ Frontend/exeryze/app/src/main/java/com/example/exercyze/MainActivity.java
@@ -9,17 +9,21 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.exercyze.app.AppController;
 
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity<stringRequest> extends AppCompatActivity  {
 
     private Button bLogout;
     private EditText etName, etAge, etUsername, etPassword;
@@ -32,7 +36,7 @@ public class MainActivity extends AppCompatActivity  {
         etName = (EditText) findViewById(R.id.etName);
         etAge = (EditText) findViewById(R.id.etAge);
         etUsername = (EditText) findViewById(R.id.etUsername);
-
+        final TextView textView = (TextView) findViewById(R.id.text);
         bLogout = (Button) findViewById(R.id.bLogout);
 
         bLogout.setOnClickListener(new View.OnClickListener(){
@@ -45,6 +49,26 @@ public class MainActivity extends AppCompatActivity  {
                 }
             }
         });
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://coms-309-sb-7.misc.iastate.edu:8080/api/user";
+
+        // Request a string response from the provided URL.
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                public void onResponse(JSONObject response){
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response.toString());
+                    }
+                },new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
     }
 
     public void openLogin()
@@ -54,23 +78,42 @@ public class MainActivity extends AppCompatActivity  {
     }
 
 
-    // Tag used to cancel the request
-    String tag_json_obj ="json_obj_req";
-    String url ="https://coms-309-sb-7.misc.iastate.edu/api/users";
+//    // Tag used to cancel the request
+//    String tag_json_obj ="json_obj_req";
+//    String url ="http://coms-309-sb-7.misc.iastate.edu:8080/api/user";
+//
+//    JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+//        @Override
+//                public void onResponse(JSONObject response){
+//            Log.d(TAG, response.toString());
+//        }
+//    },new Response.ErrorListener() {
+//        @Override
+//                public void onErrorResponse(VolleyError error) {
+//            VolleyLog.d(TAG,"Error: "+ error.getMessage());
+//
+//        }
+//    });
+//      //  AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
 
-    JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-        @Override
-                public void onResponse(JSONObject response){
-            Log.d(TAG, response.toString());
-        }
-    },new Response.ErrorListener() {
-        @Override
-                public void onErrorResponse(VolleyError error) {
-            VolleyLog.d(TAG,"Error: "+ error.getMessage());
 
-        }
-    });
-        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
-
+//    // Instantiate the RequestQueue.
+//    RequestQueue queue = Volley.newRequestQueue(this);
+//    String url ="http://coms-309-sb-7.misc.iastate.edu:8080/api/user";
+//
+//    // Request a string response from the provided URL.
+//    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//            new Response.Listener<String>() {
+//                @Override
+//                public void onResponse(String response) {
+//                    // Display the first 500 characters of the response string.
+//                    textView.setText("Response is: "+ response.substring(0,500));
+//                }
+//            }, new Response.ErrorListener() {
+//        @Override
+//        public void onErrorResponse(VolleyError error) {
+//            textView.setText("That didn't work!");
+//        }
+//    });
 
 }
