2c71dca145eae2bee54778c99304e92e63ce96bd
Dylan Smith <dylan1999.smith@gmail.com>
2019-10-09 15:19:32 -0500

    Setup app view

--- Experiments/DylanSmithExperiment/FrontendExperiment/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ Experiments/DylanSmithExperiment/FrontendExperiment/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addBtn = (Button) findViewById(R.id.addBtn);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
