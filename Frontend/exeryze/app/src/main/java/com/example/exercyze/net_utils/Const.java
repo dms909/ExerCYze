0b85db0c7ac77dd6180478acffe742deb19d45a7
mspatel <mspatel@iastate.edu>
2019-10-11 15:44:56 -0500

    added app and net_util pacakage to master for it never had

---
+++ Frontend/exeryze/app/src/main/java/com/example/exercyze/net_utils/Const.java
@@ -0,0 +1,8 @@
+package com.example.exercyze.net_utils;
+
+public class Const {
+    public static final String URL_JSON_OBJECT = "http://coms-309-sb-7.misc.iastate.edu:8080/api/user";
+    public static final String URL_JSON_ARRAY = "http://coms-309-sb-7.misc.iastate.edu:8080/api/user";
+    public static final String URL_STRING_REG = "http://coms-309-sb-7.misc.iastate.edu:8080/api/user";
+    public static final String URL_IMAGE = "https://api.androidhive.info/volley/volley-image.jpg";
+}
